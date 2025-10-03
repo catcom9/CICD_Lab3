2d2e947ef9b8cf24a5aee2107c4fb58a483a826b
Cathal Commins <G00427615@atu.ie>
2025-10-03 09:40:54 +0100

    Created a bulk add endpoint and a endpoint to view everything

--- src/main/java/ie/atu/week3/Product.java
+++ src/main/java/ie/atu/week3/Product.java
@@ -1,12 +1,34 @@
 package ie.atu.week3;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Data
-@AllArgsConstructor
+//@Data
+//@AllArgsConstructor
 public class Product {
     private String name;
     private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
